ccb5b98fd1b9320928f6e9e005687223a549301e
Surya Gaddipati <sgaddipati@groupon.com>
2014-08-01 09:40:06 -0500

    Dont attempt to copyfiles if plugin doesn't need to

---
+++ src/test/java/com/groupon/jenkins/dynamic/buildconfiguration/plugins/DotCiPluginAdapterTest.java
@@ -0,0 +1,52 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014, Groupon, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.groupon.jenkins.dynamic.buildconfiguration.plugins;
+
+import com.groupon.jenkins.dynamic.build.DynamicBuild;
+import com.groupon.jenkins.dynamic.build.DynamicSubBuild;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import java.io.IOException;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+public class DotCiPluginAdapterTest {
+
+
+    @Test
+    public void should_copy_files_only_when_specified() throws IOException {
+        DotCiPluginAdapter plugin =spy( new DotCiPluginAdapter("myplugin", "") {
+
+            @Override
+            public boolean perform(DynamicBuild dynamicBuild, Launcher launcher, BuildListener listener) {
+                return true;
+            }
+        });
+
+        plugin.runFinished(null,null,null);
+        verify(plugin,never()).copyFiles(any(DynamicSubBuild.class),any(DynamicBuild.class),anyString(),any(BuildListener.class));
+    }
+
+}
