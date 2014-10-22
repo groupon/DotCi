953f8e79d838307bbde6a897f13534bff8de113f
Greg Blike <gblike@groupon.com>
2014-10-22 16:32:16 -0500

    Update which fields need to be removed

--- src/main/groovy/doci_db_xml_morphia_migration.groovy
+++ src/main/groovy/doci_db_xml_morphia_migration.groovy
@@ -2,6 +2,7 @@ import com.groupon.jenkins.dynamic.build.repository.*;
 import com.mongodb.BasicDBObject;
 import com.groupon.jenkins.dynamic.organizationcontainer.OrganizationContainer;
 import hudson.util.XStream2;
+import com.groupon.jenkins.SetupConfig;
 import groovy.xml.*;
 
 def migrate_build(project, db_build_obj) {
@@ -11,9 +12,7 @@ def migrate_build(project, db_build_obj) {
 
     def root = new XmlSlurper().parseText(db_build_obj.get("xml"))
     root.dynamicBuildLayouter.replaceNode{}
-    root.actions."com.groupon.jenkins.dynamic.build.execution.SubBuildExecutionAction".replaceNode{}
-    root.actions."org.jenkinsci.plugins.envinject.EnvInjectPluginAction".replaceNode{}
-    root.actions."jenkins.plugins.show__build__parameters.ShowParametersBuildAction".replaceNode{}
+
     def doc = XmlUtil.serialize(root)
 
     def build = xml_processor.fromXML(doc)
@@ -22,7 +21,7 @@ def migrate_build(project, db_build_obj) {
     return build
   } catch(e) {
     println("Unable to migrate build: ${db_build_obj.get("_id")}")
-    throw(e)
+    //throw(e)
   }
 }
 
@@ -34,13 +33,14 @@ def migrate_project(parent, db_project_obj) {
     def root = new XmlSlurper().parseText(db_project_obj.get("xml"))
     root.dynamicProjectRepository.replaceNode{}
     root.dynamicBuildRepository.replaceNode{}
+
     def doc = XmlUtil.serialize(root)
 
     def project = xml_processor.fromXML(doc)
     project.setId(db_project_obj.get("_id"))
     project.onLoad(parent, db_project_obj.get("name"))
 
-    def build_coll = new DynamicBuildRepository().getDatastore().getDB().getCollection("dotci_build")
+    def build_coll = SetupConfig.get().getDynamicBuildRepository().getDatastore().getDB().getCollection("dotci_build")
     def builds = build_coll.find(new BasicDBObject("parent", project.getId()))
     for(build_obj in builds) {
       println("Migrating Build: ${project.getName()} [Num: ${build_obj.get("number")} ID:${build_obj.get("_id")}]")
@@ -52,7 +52,7 @@ def migrate_project(parent, db_project_obj) {
     return project
   } catch(e) {
     println("Unable to migrate project: ${db_project_obj.get("_id")}")
-    throw(e)
+    //throw(e)
   }
 }
 
@@ -61,15 +61,16 @@ def org_containers = Jenkins.getInstance().getItems(OrganizationContainer.class)
 for(container in org_containers) {
   container.save()
   println("Migrating Container: " + container.getName())
-  def job_coll = new DynamicBuildRepository().getDatastore().getDB().getCollection("dotci_project")
+  def job_coll = SetupConfig.get().getDynamicBuildRepository().getDatastore().getDB().getCollection("dotci_project")
   def child_projects = job_coll.find(new BasicDBObject("parent", container.getName()) )
   for(db_project_obj in child_projects) {
     println("Migrating Project: ${db_project_obj.get("name")} [ID: ${db_project_obj.get("_id")}]")
 
     def project = migrate_project(container, db_project_obj)
-    project.save()
 
     if(project != null) {
+      project.save()
+
       def subprojects = job_coll.find(new BasicDBObject("parent", project.getId()))
       for(subproject_obj in subprojects) {
         println("Migrating SubProject: ${db_project_obj.get("name")} [ID: ${db_project_obj.get("_id")}]")
