0ebaa1c7f72a2d5e741cb8760b180e6f45c71bab
Surya Gaddipati <sgaddipati@groupon.com>
2015-02-21 14:04:10 -0600

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,12 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:[' +
+                '"Android 2.3", "Android >= 4", "Chrome >= 20", "Firefox >= 24", ' +
+                '"Explorer >= 8", "iOS >= 6", "Opera >= 12", "Safari >= 6"]}'
+            },
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
