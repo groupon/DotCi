40104f17c8356f8c1c32a3f2c09237b7ba9b660c
Surya Gaddipati <sgaddipati@groupon.com>
2015-02-22 12:33:04 -0600

    Add jshint

--- src/main/jsx/actions/Actions.jsx
+++ src/main/jsx/actions/Actions.jsx
@@ -21,9 +21,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+'use strict'
 import { Actions } from 'flummox';
-import {recentProjects,job} from "../api/Api.jsx"
+import {recentProjects,job} from "../api/Api.jsx";
 import jQuery from "jquery";
 
 
