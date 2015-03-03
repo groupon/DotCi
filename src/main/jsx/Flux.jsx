1a315e17f575e484acd5b1d681870bcb3d0fe12e
Surya Gaddipati <sgaddipati@groupon.com>
2015-03-02 19:47:48 -0600

    use watch

--- src/main/jsx/Flux.jsx
+++ src/main/jsx/Flux.jsx
@@ -1,11 +1,9 @@
-'use strict';
 
 import Flummox from 'flummox';
 import AppActions from './actions/Actions.jsx';
 import RecentProjectsStore from './stores/RecentProjectsStore.jsx';
 import JobStore from './stores/JobStore.jsx';
  export default class Flux extends Flummox {
-
     constructor() {
         super();
         this.createActions('app', AppActions);
