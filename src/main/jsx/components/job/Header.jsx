a781034179acb181f6a4ddf6c96514202feae636
Surya Gaddipati <sgaddipati@groupon.com>
2015-03-09 20:35:40 -0500

    Add configure menu

--- src/main/jsx/components/job/Header.jsx
+++ src/main/jsx/components/job/Header.jsx
@@ -7,10 +7,21 @@ export default React.createClass({
           <div className="flex-row">
                     <IconLink href={this.props.githubUrl} icon="octicon octicon-mark-github">{this.props.fullName}</IconLink>
                     <IconLink href="build?delay=0sec" icon="fa fa-rocket"> Build Now</IconLink>
+<div className="ui compact menu">
+    <div className="button labeled ui simple dropdown item">
+      <i className="dropdown icon"></i>
+      <i className="icon fa fa-cog"/>  Settings
+      <div className="fa-stack menu">
+        <a  href="configure" className="ui labeled item"><i className="icon fa fa-wrench "/> Configure</a>
+        <a  href="#" onClick={this._deleteJob} className="ui labeled item"><i className="icon fa fa-trash-o "/>Delete</a>
+      </div>
+    </div>
+  </div>
+
             </div>
         );
     },
-    deleteJob(){
+    _deleteJob(){
         let actions = this.props.flux.getActions('app');
         actions.deleteProject();
     }
