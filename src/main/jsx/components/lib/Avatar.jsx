4742c0d895033ccff99fa236dd2456c69a32f108
Surya Gaddipati <sgaddipati@groupon.com>
2015-03-19 10:28:39 -0500

    Specify dimensions for avatars

--- src/main/jsx/components/lib/Avatar.jsx
+++ src/main/jsx/components/lib/Avatar.jsx
@@ -25,6 +25,6 @@
 import React from 'react';
 export default React.createClass({
   render(){
-      return <img  className="ui avatar image" alt={this.props.emailDigest}  src={"https://secure.gravatar.com/avatar/"+this.props.emailDigest+".png"}/>;
+      return <img height="23" width="23" className="ui avatar image" alt={this.props.emailDigest}  src={"https://secure.gravatar.com/avatar/"+this.props.emailDigest+".png?s=23"}/>;
   }
 });
