1e6f0c3db993058d4d5618c97b69cc2ce9711eb0
Surya Gaddipati <sgaddipati@groupon.com>
2015-04-03 10:56:55 -0500

    Add build actions to build page

--- src/main/jsx/components/job/build/Build.jsx
+++ src/main/jsx/components/job/build/Build.jsx
@@ -4,12 +4,26 @@ import BuildRow from '../BuildRow.jsx'
 export default  React.createClass({
   componentDidMount(){
     const actions = this.props.flux.getActions('app');
-     actions.currentBuildChanged(this.props.url);
+    actions.currentBuildChanged(this.props.url);
   },
   render(){
     return this.props.build? (<div>
       <BuildRow  {...this.props.build}/>
+      {this._buildActions()}
       <Console {...this.props} />
-  </div>):<div/>;
+    </div>):<div/>;
+  },
+  _buildActions(){
+    return <div>
+      {this.props.cancelable? this._cancelButton():''}
+      <div className="circular ui icon button">
+        <i className="fa fa-times-circle-o"></i>
+      </div>
+    </div>
+  },
+  _cancelButton(){
+    return <div className="circular ui icon button">
+      <i className="fa fa-refresh"></i>
+    </div>
   }
 });
