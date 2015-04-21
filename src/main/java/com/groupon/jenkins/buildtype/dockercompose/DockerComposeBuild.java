ba49ae80c311122bbfb0a727e64ba0e0f2b1dc96
Surya Gaddipati <sgaddipati@groupon.com>
2015-04-21 15:41:49 -0500

    Change permissions before delete

--- src/main/java/com/groupon/jenkins/buildtype/dockercompose/DockerComposeBuild.java
+++ src/main/java/com/groupon/jenkins/buildtype/dockercompose/DockerComposeBuild.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014, Groupon, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.groupon.jenkins.buildtype.dockercompose;
 
 import com.google.common.collect.ImmutableMap;
@@ -66,7 +90,7 @@ public class DockerComposeBuild extends BuildType implements SubBuildRunner {
         boolean isPrivateRepo = Boolean.parseBoolean((String) dotCiEnvVars.get("DOTCI_IS_PRIVATE_REPO"));
         String gitUrl = gitRepoUrl.getGitUrl();
         ShellCommands shellCommands = new ShellCommands();
-        shellCommands.add("find . ! -path \"./deploykey_rsa.pub\" ! -path \"./deploykey_rsa\" -delete");
+        shellCommands.add("chmod -R u+w . ; find . ! -path \"./deploykey_rsa.pub\" ! -path \"./deploykey_rsa\" -delete");
         shellCommands.add("git init");
         shellCommands.add(format("git remote add origin %s",gitUrl));
 
