a024fc9388e3fdd154db3ba6e47c523488f5ec7d
Surya Gaddipati <sgaddipati@groupon.com>
2015-04-29 15:25:53 -0500

    Add notify button for web notifications

--- src/main/jsx/components/job/build/Build.jsx
+++ src/main/jsx/components/job/build/Build.jsx
@@ -3,6 +3,7 @@ import Console from './Console.jsx';
 import BuildRow from '../BuildRow.jsx'
 import ActionButton from './../../lib/ActionButton.jsx';
 import FaviconHelper from './../../mixins/FaviconHelper.jsx';
+import simpleStorage from './../../../vendor/simpleStorage.js';
 require('./build.less');
 export default  React.createClass({
   mixins: [FaviconHelper],
@@ -12,10 +13,11 @@ export default  React.createClass({
   componentDidUpdate(){
     if(this._getBuildResult() == 'IN_PROGRESS' ){
       this._setRefreshTimer();
-      this.setFavicon(this._getBuildResult());
     }else{
       this._clearRefreshTimer();
+      this._webNotifyCompletion();
     }
+    this.setFavicon(this._getBuildResult());
   },
   componentWillUnmount: function() {
     this._clearRefreshTimer(); 
@@ -43,7 +45,32 @@ export default  React.createClass({
     </div>):<div/>;
   },
   _buildActions(){
-    return this.props.build.get('cancelable')? this._cancelButton():this._restartButton();
+    return  this.props.build.get('cancelable')? this._inProgressActions():[this._restartButton()];
+  },
+  _inProgressActions(){
+    return [this._cancelButton(), this_watchButton()];
+  },
+  _watchButton(){
+    return this._supportsNotifications()?<ActionButton ref="watchButton" onClick={this._watchBuild} tooltip="Notify when done( web notification)" icon="fa fa-eye"/>: <span/>;
+  },
+  _supportsNotifications(){
+    return simpleStorage.canUse() && ("Notification" in window);
+  },
+  _watchBuild(e){
+    Notification.requestPermission((result)=>{
+      if(result == 'granted'){
+        this.refs.watchButton.disable();
+        const url = this.props.build.get('cancelUrl')
+        simpleStorage.set(url, true, {TTL: 7200000}) // 2 hrs
+      }
+    });
+  },
+  _webNotifyCompletion(){
+    const url = this.props.build && this.props.build.get('cancelUrl')
+    if(simpleStorage.get(url)){
+      new Notification(`Build #${this._get('number')} finished with ${this._get('result')}`, {body:`${this._get('commit').get('message')}`, icon: "<i class='fa fa-bed'></i>"});
+      simpleStorage.deleteKey(url);
+    }
   },
   _restartButton(){
     return <ActionButton onClick={this._restartBuild} tooltip="Restart Build" icon="fa fa-refresh" />;
@@ -59,5 +86,8 @@ export default  React.createClass({
   _restartBuild(e){
     e.preventDefault();
     window.location = window.location + '/rebuild/parameterized';
+  },
+  _get(key){
+    return this.props.build.get(key)
   }
 });
