7db7fb322c88efb1ad46381c394ba0ea7d9827b4
Surya Gaddipati <sgaddipati@groupon.com>
2015-05-14 15:15:35 -0500

    Group by sha instead of commit

--- src/main/jsx/components/job/GroupedBuildsView.jsx
+++ src/main/jsx/components/job/GroupedBuildsView.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LinearBuildsView from './LinearBuildsView.jsx';
 export default React.createClass({
   render(){
-    const groupedBuilds = this.props.builds.groupBy(x => x.get('commit'));
+    const groupedBuilds = this.props.builds.groupBy(x => x.get('commit').get('shortSha'));
     const buildGroups = groupedBuilds.map((builds,commit) => {
       return(<div key={commit} className="ui segment">
         <LinearBuildsView  builds={builds}/> 
