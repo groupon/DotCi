8f021b3938d40c1cb835dfcfe82076fa5c2b416b
Surya Gaddipati <sgaddipati@groupon.com>
2015-05-17 20:22:26 -0500

    Add css transition for builds view

--- src/main/jsx/components/job/LinearBuildsView.jsx
+++ src/main/jsx/components/job/LinearBuildsView.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import BuildRow from './BuildRow.jsx';
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+require('./linear_builds_view.css')
 export default React.createClass({
   render(){
     const builds = this.props.builds.map((build) => <BuildRow key={build.get('number')} build={build}/>);
-    return(<span className="builds">{builds.toArray()}</span>);
+    return(<span className="builds">
+      <ReactCSSTransitionGroup transitionName="build-transition">
+        {builds.toArray()}
+      </ReactCSSTransitionGroup>
+    </span>);
   }
 });
 
