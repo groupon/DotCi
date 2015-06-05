fa6e09bfcf53adc23dede9f6a5b06f449bda9513
Surya Gaddipati <sgaddipati@groupon.com>
2015-06-05 13:42:20 -0500

    Move BranchTabs into a seperate Action

--- src/main/jsx/components/job/BuildHistory.jsx
+++ src/main/jsx/components/job/BuildHistory.jsx
@@ -23,12 +23,7 @@
  */
 
 import React from 'react';
-import ActionButton from './../lib/ActionButton.jsx';
 import BuildRow from './BuildRow.jsx';
-import Dialog from './../lib/Dialog.jsx';
-import contains from 'ramda/src/contains'
-import classNames from 'classnames'; 
-import LocationHashHelper from './../mixins/LocationHashHelper.jsx'
 import Router from 'react-router';
 import FilterBar from './../FilterBar.jsx';
 import LoadingHelper from './../mixins/LoadingHelper.jsx';
@@ -37,6 +32,7 @@ import GroupedBuildsView from './GroupedBuildsView.jsx';
 import LinearBuildsView from './LinearBuildsView.jsx';
 import ToggleButton from './../lib/ToggleButton.jsx';
 import RangeSlider from './../lib/RangeSlider.jsx';
+import BranchTabs from './BranchTabs.jsx';
 require('./build_history.less');
 var BuildHistoryTable = React.createClass({
   getInitialState: function() {
@@ -68,66 +64,6 @@ var BuildHistoryTable = React.createClass({
     this.setState({filter});
   }
 });
-var BuildHistoryTabs = React.createClass({
-  mixins: [LocationHashHelper], 
-  getInitialState(){
-    return {currentSelection: this.selectedHash()?this.selectedHash(): this.props.defaultTab};
-  },
-  render()  {
-    return (<div className="ui text menu">
-      <div className="ui item"><ActionButton tooltip="Build Now" href="build?delay=0sec" icon="fa fa-rocket" primary/></div>
-      {this.props.tabs.map((tab,i)=>this._getHistoryTab(tab,i,this._isTabRemovable(tab))).toArray()}
-      <ActionButton className="ui item" tooltip="Add new tab" onClick={this._addTab} icon="fa fa-plus-circle"/>
-      <Dialog ref="addDialog" title="Add new brach tab" onSave={this._onTabSave} >
-        <div className="ui labeled input">
-          <div className="ui label">
-            Branch Regex
-          </div>
-          <input type="text" ref="newBranchTab" placeholder=""/>
-        </div>
-      </Dialog>
-    </div>);
-  },
-  _isTabRemovable(tab){
-    return !contains(tab)(['master','All','Mine']);
-  },
-  _addTab(){
-    const addDialog = this.refs.addDialog;
-    addDialog.open();
-  },
-  _onTabSave(){
-    const tabExpr = this.refs.newBranchTab.getDOMNode().value
-    this.props.flux.addBranchTab(tabExpr);
-  },
-  _notifyTabSelection: function (tab) {
-    this.replaceState({currentSelection: tab});
-    this.props.onTabChange(tab);
-  },
-  _onLocationHashChange(event){
-    const selectedTab = this.selectedHash();
-    this._notifyTabSelection(selectedTab?selectedTab:this.defaultTab);
-  },
-  _onTabRemove(event){
-    event.stopPropagation();
-    this.replaceState(this.getInitialState());
-    this._notifyTabSelection(this.state.currentSelection);
-    var tab = event.currentTarget.getAttribute('data-tab');
-    this.props.flux.removeBranchTab(tab);
-  },
-  _getHistoryTab(tab,i,closable) {
-    var classes = classNames({
-      'ui':true,
-      'item':true ,
-      'green':true ,
-      'active': this.state.currentSelection== tab
-    });
-    return (<a className={classes} key={i} href={'#'+tab}>
-      <i className="icon octicon octicon-git-branch "></i>
-      {tab}
-      {closable?<div data-tab={tab} className="tab-close fa fa-times-circle-o" onClick={this._onTabRemove}></div>: ''}
-    </a>);
-  }
-});
 
 export default React.createClass({
   mixins:[LoadingHelper,AutoRefreshHelper],
@@ -147,7 +83,7 @@ export default React.createClass({
   _render(){
     const countSlider = <RangeSlider ref="buildCount" tooltip="Build count" queryParam="count" onChange={this._onCountChange} min={20}  max={100} step={5}  />
     return(<div id="build-history">
-      <BuildHistoryTabs  onTabChange={this._onTabChange} flux={this.props.flux} tabs={this.props.tabs} defaultTab={this.defaultTab}/>
+      <BranchTabs  onTabChange={this._onTabChange} flux={this.props.flux} tabs={this.props.tabs} defaultTab={this.defaultTab}/>
       <BuildHistoryTable countSlider={countSlider} builds ={this.props.builds}/>
     </div>);
   },
