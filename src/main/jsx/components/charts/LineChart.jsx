90b44d5b62de6695ad01a6e65fab88f64f0e81e5
Surya Gaddipati <sgaddipati@groupon.com>
2015-06-09 18:26:01 -0500

    Select individual datasets on a graph

--- src/main/jsx/components/charts/LineChart.jsx
+++ src/main/jsx/components/charts/LineChart.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Chart from 'chart.js';
 import  LoadingHelper from './../mixins/LoadingHelper.jsx';
+import {Set} from 'immutable';
 require('./line_chart.css');
 export default React.createClass({
   mixins:[LoadingHelper],
+  getInitialState(){
+    return {unchecked: Set()};
+  },
   componentDidUpdate(){
     this._renderChart();
   },
@@ -13,18 +17,24 @@ export default React.createClass({
   _renderChart(){
     const {dataSets,labels} = this.props.chart.toObject();
     const chartCtx = this.refs.chart.getDOMNode().getContext('2d');
-    var chartData = { labels : labels.toJS(), datasets: dataSets.toJS() };
-    this.chart = new Chart(chartCtx).Line(chartData, {scaleShowGridLines : false,legendTemplate: this._legend()});
-    this.refs.legend.getDOMNode().innerHTML = this.chart.generateLegend();
-    this.chart.update();
+    const filteredDataSet = this._removeUnchecked(dataSets);
+    if(filteredDataSet.size == 0){
+      var chartData = { labels :[], datasets: [] };
+    }else{
+      var chartData = { labels : labels.toJS(), datasets: filteredDataSet.toJS() };
+      new Chart(chartCtx).Line(chartData, {scaleShowGridLines : false});
+    }
   },
-  _onClick(e){
+  _removeUnchecked(dataSets){
+    return dataSets.filter(dataSet => !this.state.unchecked.has(dataSet.get('label')));
   },
   _render(){
     let {xLabel,yLabel} = this.props.chart.toObject();
     return (
       <div className="chart-container">
-        <div  ref="legend"></div>
+        <div>
+          {this._legend()}
+        </div>
         <h5 className="axis-label yaxis-label">{yLabel}</h5>
         <span>
           <h4 className="align-center">{this.props.title}</h4>
@@ -34,15 +44,24 @@ export default React.createClass({
       </div>);
   },
   _legend(){
-    return `<ul class=\"<%=name.toLowerCase()%>-legend\">
-    <% for (var i=0; i<datasets.length; i++){%>
-    <li>
-    <span style=\"color:<%=datasets[i].strokeColor%>\">
-    <%if(datasets[i].label){%>
-    <%=datasets[i].label%>
-    <%}%>
-    </li></span>
-    <%}%>
-    </ul>`
+    const {dataSets} = this.props.chart.toObject();
+    return  <div className="legend">
+      {dataSets.map(dataSet=> <div key={dataSet.get('label')} style={{color: dataSet.get('strokeColor') }} >
+        <input type="checkbox" name={dataSet.get('label')}  onChange={this._dataSetChanged} checked={this._isSelected(dataSet.get('label'))}/>{dataSet.get('label')}
+      </div>)}
+    </div>
+  },
+  _isSelected(label){
+    return !this.state.unchecked.has(label);
+  },
+  _dataSetChanged(e){
+    const targetLabel =e.target.getAttribute('name');
+    const {dataSets} = this.props.chart.toObject();
+    if(!e.target.checked) {
+      this.replaceState({unchecked: this.state.unchecked.add(targetLabel)});
+    }else{
+      this.replaceState({unchecked: this.state.unchecked.delete(targetLabel)});
+    }
   }
+
 })
