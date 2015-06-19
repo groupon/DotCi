b9af86e4d43143ec6654e3b74a8201ba78e6dcd7
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-06-19 14:42:02 -0500

    Add mobile header

--- src/main/jsx/AppMobile.jsx
+++ src/main/jsx/AppMobile.jsx
@@ -2,32 +2,33 @@ import React from "react";
 import FluxComponent from 'flummox/component';
 import RecentProjects from "./components/recent_projects/RecentProjects.jsx";
 import {RouteHandler} from 'react-router';
+import EmptyProject from './EmptyProject.jsx';
 require('./css/ratchet.min.css');
+require('./app-mobile.css');
 export default  React.createClass({
   getInitialState(){
     return {active: "job"}
   },
   render(){
-    return  <div>
-      <div className="content">
+    return  <div><div className="content">
         <div className="card">
           {this._content()}
         </div>
-        <nav className="bar bar-tab">
-          <a data-tab="job" 
-            className= {"tab-item "+ (this.state.active === 'job'? 'active': '')} 
-            onClick={this._tabClick} href="#">
-            <span className="icon icon-list"></span>
-            <span className="tab-label">Job</span>
-          </a>
-          <a data-tab="recent-projects" 
-            className= {"tab-item "+ (this.state.active === 'recent-projects'? 'active': '')} 
-            onClick={this._tabClick} href="#">
-            <span className="icon icon-person"></span>
-            <span className="tab-label">Your History</span>
-          </a>
-        </nav>
-      </div> 
+      </div>
+      <nav className="bar bar-tab">
+        <a data-tab="job" 
+          className= {"tab-item "+ (this.state.active === 'job'? 'active': '')} 
+          onClick={this._tabClick} href="#">
+          <span className="icon icon-list"></span>
+          <span className="tab-label">Job</span>
+        </a>
+        <a data-tab="recent-projects" 
+          className= {"tab-item "+ (this.state.active === 'recent-projects'? 'active': '')} 
+          onClick={this._tabClick} href="#">
+          <span className="icon icon-person"></span>
+          <span className="tab-label">Your History</span>
+        </a>
+      </nav>
     </div>
   },
   _tabClick(e){
