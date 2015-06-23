058c0e4e1813574de6316a8fb6c4bd6e9bd77c0b
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-06-23 13:40:06 -0500

    Move settings menu into header

--- src/main/jsx/components/lib/Widgets.jsx
+++ src/main/jsx/components/lib/Widgets.jsx
@@ -24,20 +24,15 @@
 import React from 'react';
 import filter from 'ramda/src/filter';
 import Router from 'react-router';
-import SettingsMenu from './../job/SettingsMenu.jsx';
 export default React.createClass({
   render(){
     const activeWidget = this._activeWidget();
-    const settingsMenu = <SettingsMenu job={this.props.job} flux={this.props.flux}/>
     var navs = filter(widget => !widget.props.tabVisibleWhenActive || this._isWidgetSelected(widget), this.props.children).map((widget,index) => this._tabItem(widget,index,widget===activeWidget));
     return(
       <div>
         <div ref="activeWidget">
           <div className="ui secondary pointing menu">
             {navs}
-            <div className="right menu">
-              {settingsMenu}
-            </div>
           </div>
           {activeWidget}
         </div>
