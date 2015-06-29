7051f0669a90bfea29e2012e587b6930761a268a
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-06-29 13:21:48 -0500

    use paper input for filter input

--- src/main/jsx/components/FilterBar.jsx
+++ src/main/jsx/components/FilterBar.jsx
@@ -1,13 +1,7 @@
-
 import React from 'react';
 export default  React.createClass({
   render(){
-    return (
-      <div id={this.props.id} className="ui icon input">
-        <input type="text" onChange={this._onChange} placeholder="Filter..."/>
-        <i className="fa fa-filter icon"></i>
-      </div>
-    );
+    return <paper-input label="Filter... " onInput={this._onChange}></paper-input>
   },
   _onChange(event){
     this.props.onChange(event.target.value);
