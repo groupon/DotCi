d557875918b9c2d33b15cd4b9727ae38afe75f23
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-06-29 16:54:19 -0500

    Use paper tabs for subbuilds

--- src/main/jsx/components/job/build/SubBuildsMenu.jsx
+++ src/main/jsx/components/job/build/SubBuildsMenu.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Router from 'react-router';
 import Immutable from 'immutable';
+import CustomAttributes from './../../mixins/CustomAttributes.jsx';
 import BuildProgressBar from './../BuildProgressBar.jsx';
 require('./sub-builds-menu.css');
 export default  React.createClass({
+  mixins: [CustomAttributes],
   render(){
-    return this._isMultiConfig()? 
-      <div className="ui secondary pointing menu">
+    if(this._isMultiConfig()){
+      const selected = this.props.axisList.findIndex(subBuild => this.props.selectedBuild === subBuild.get('script'))
+      return <paper-tabs ref="ca-subbuilds" attrs={{selected}}>
         {this._subBuilds()}
-      </div> : <span/>;
+      </paper-tabs> 
+    }
+
+    return <span/>;
   },
   _isMultiConfig(){
     return this.props.axisList.count() > 1;
@@ -16,14 +22,13 @@ export default  React.createClass({
   _subBuilds(){
     return this.props.axisList.map(subBuild =>{
       const build = subBuild.get('script');
-      const classes = " item " + (this.props.selectedBuild === build? "active" : '');
-      return <span key={build} className="item">
+      return <paper-tab key={build}>
         <BuildProgressBar build={subBuild} />
-        <Router.Link className={classes}   href='#' to="job-widgets-param" params={{widget:this.props.buildNumber , param: 'dotCI'+build}}>
+        <Router.Link   href='#' to="job-widgets-param" params={{widget:this.props.buildNumber , param: 'dotCI'+build}}>
           <span className= {"subbuild-"+ subBuild.get('result')}>{build}</span>
           <span className="subbuild-duration">{subBuild.get('duration')?Math.floor(subBuild.get('duration')/ 60000) +' mins': '-'}</span> 
         </Router.Link>
-      </span>
+      </paper-tab>
     }); 
   }
 });
