e722307a3fc15a094e239d3018ee8e54a0f1e4fb
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-06-29 16:35:37 -0500

    Toggle to pipeline view

--- src/main/jsx/components/lib/ToggleButton.jsx
+++ src/main/jsx/components/lib/ToggleButton.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 export default React.createClass({
   render(){
     return <div data-hint={this.props.tooltip} className="hint--bottom ">
-      <paper-toggle-button onClick={this._onChange}/>
-      {this.props.children}
+      <paper-toggle-button onClick={this._onClick}/>
     </div>
+  },
+  _onClick(e){
+    this.props.onClick(e.currentTarget.checked);
   }
 });
