180d9fc11492dabf579851cfd24ebf89edf18991
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-14 18:13:40 -0500

    Query change

--- src/main/jsx/models/BuildHistory.js
+++ src/main/jsx/models/BuildHistory.js
@@ -1,19 +1,19 @@
+import {job} from './../api/Api.jsx'; 
 export default class {
   constructor(){
     this.filters= [];
     this.builds= [];
-    this.buildCount =50;
-    this.buildFilter ='All';
   }
   addChangeListener(changeListener){
     this.changeListener = changeListener;
   }
-  filterChange(){
-  }
-  buildCountChange(){
-  }
   historyChanged(change){
     Object.assign(this,change);
     this.changeListener(this);
+  }
+  queryChange(query){
+    job("buildHistoryTabs,builds[*,commit[*],cause[*],parameters[*]]", query.filter,query.limit).then(data => {
+      this.historyChanged({... data, filters: data.buildHistoryTabs});
+    });
   }
 }
