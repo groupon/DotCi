3fd31331f69c2a546c85e3e36d07ced0793116ea
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-15 11:58:54 -0500

    Change method names

--- src/main/jsx/models/BuildHistory.js
+++ src/main/jsx/models/BuildHistory.js
@@ -1,18 +1,23 @@
+/*
+ * Model has 
+ * 1.data 
+ * 2. query that corrsponds to that data
+ */
 export default class {
   constructor(){
     this.filters= [];
     this.builds= [];
     this.query={}
   }
-  addChangeListener(changeListener){
-    this.changeListener = changeListener;
+  addDataChangeListener(changeListener){
+    this.dataChangeListener = changeListener;
   }
   addQueryChangeListener(queryChangeListener){
     this.queryChangeListener = queryChangeListener;
   }
-  historyChanged(change){
+  dataChanged(change){
     Object.assign(this,change);
-    this.changeListener(this);
+    this.dataChangeListener(this);
   }
   queryChanged(query){
     Object.assign(this.query,query);
