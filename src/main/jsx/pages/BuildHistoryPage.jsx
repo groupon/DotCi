0383542b580767965738986d69d6b85ff73c4200
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-15 11:10:50 -0500

    Pass in query filters

--- src/main/jsx/pages/BuildHistoryPage.jsx
+++ src/main/jsx/pages/BuildHistoryPage.jsx
@@ -6,7 +6,7 @@ export default React.createClass({
   render(){
     const {filters,builds,filterChange,buildCountChange} = this.props.buildHistory;
     const countSlider = <RangeSlider ref="buildCount" tooltip="Build count"  onChange={buildCountChange} min={20}  max={100} step={5}  />;
-    const buildFilters = <BuildFilters  ref="branchTabs" onFilterChange={filterChange}  filters={filters} />;
+    const buildFilters = <BuildFilters  ref="branchTabs"  buildHistory={this.props.buildHistory} />;
     return(<div className="align-center" >
       <BuildHistoryTable buildFilters={buildFilters} countSlider={countSlider} builds ={builds}/>
     </div>);
