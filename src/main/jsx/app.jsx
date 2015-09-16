b110c5fb6648026621e8230ef021e3227544d7c1
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-16 10:50:27 -0500

    Abstract Action to its own object

--- src/main/jsx/app.jsx
+++ src/main/jsx/app.jsx
@@ -3,20 +3,24 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import BuildHistoryPage from './pages/BuildHistoryPage.jsx';
 import {job} from './api/Api.jsx'; 
-import BuildHistory from './models/BuildHistory.js'
+import {Actions,default as BuildHistory} from './models/BuildHistory.js'
 window.onload = function (){
   const buildHistory = new BuildHistory();
-  buildHistory.addDataChangeListener(buildHistory => {
-    ReactDOM.render(<BuildHistoryPage buildHistory={buildHistory}/>, document.getElementById('content'));
-  });
-  buildHistory.addQueryChangeListener(buildHistory => {
-    let query = buildHistory.query;
-    job("buildHistoryTabs,builds[*,commit[*],cause[*],parameters[*]]",query.filter ,query.limit).then(data => {
-      buildHistory.dataChanged({...data, filters: data.buildHistoryTabs});
-    });
+  buildHistory.setActionChangeListener((action,buildHistory) => {
+    switch(action.name){
+      case 'QUERY_CHANGE':
+        let query = buildHistory.query;
+      job("buildHistoryTabs,builds[*,commit[*],cause[*],parameters[*]]",query.filter ,query.limit).then(data => {
+        buildHistory.sendAction(Actions.DataChange({...data, filters: data.buildHistoryTabs}));
+      });
+      break;
+      case 'DATA_CHANGE':
+        ReactDOM.render(<BuildHistoryPage buildHistory={buildHistory}/>, document.getElementById('content'));
+      break;
+    }
   });
 
-  buildHistory.queryChanged({filter: 'All', limit: 50});
+  buildHistory.sendAction(Actions.QueryChange({filter: 'All', limit: 50}));
 
 }
 
--- src/main/jsx/models/BuildHistory.js
+++ src/main/jsx/models/BuildHistory.js
@@ -3,24 +3,32 @@
  * 1.data 
  * 2. query that corrsponds to that data
  */
+export const Actions = {
+  QueryChange(newQuery){
+    return {name:'QUERY_CHANGE', data: newQuery}
+  },
+  DataChange(newData){
+    return {name:'DATA_CHANGE', data: newData}
+  }
+}
 export default class {
   constructor(){
     this.filters= [];
     this.builds= [];
     this.query={}
   }
-  addDataChangeListener(changeListener){
-    this.dataChangeListener = changeListener;
+  setActionChangeListener(actionChangeListener){
+    this.actionChangeListener = actionChangeListener;
   }
-  addQueryChangeListener(queryChangeListener){
-    this.queryChangeListener = queryChangeListener;
-  }
-  dataChanged(change){
-    Object.assign(this,change);
-    this.dataChangeListener(this);
-  }
-  queryChanged(query){
-    Object.assign(this.query,query);
-    this.queryChangeListener(this);
+  sendAction(action){
+    switch(action.name) {
+      case 'QUERY_CHANGE':
+        Object.assign(this.query,action.data);
+      break;
+      case 'DATA_CHANGE':
+        Object.assign(this,action.data);
+      break;
+    }
+    this.actionChangeListener(action,this);
   }
 }
