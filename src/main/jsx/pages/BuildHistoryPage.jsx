445e31c9641aae9ec189d512614fc0bc52beb03a
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-16 19:06:20 -0500

    Hookup RangeSlider

--- src/main/jsx/pages/BuildHistoryPage.jsx
+++ src/main/jsx/pages/BuildHistoryPage.jsx
@@ -4,11 +4,17 @@ import BuildFilters from './../components/job/BuildFilters.jsx';
 import BuildHistoryTable from './../components/job/BuildHistoryTable.jsx';
 export default React.createClass({
   render(){
-    const {filters,builds,filterChange,buildCountChange} = this.props.buildHistory;
-    const countSlider = <RangeSlider ref="buildCount" tooltip="Build count"  onChange={buildCountChange} min={20}  max={100} step={5}  />;
-    const buildFilters = <BuildFilters  ref="branchTabs"  buildHistory={this.props.buildHistory} />;
-    return(<div className="align-center" >
-      <BuildHistoryTable buildFilters={buildFilters} countSlider={countSlider} builds ={builds}/>
-    </div>);
+    const {query,builds,actions} = this.props.buildHistory;
+    const countSlider = <RangeSlider ref="buildCount" 
+      tooltip="Build count"  
+      value={query.limit} 
+      onChange={(limit)=> actions.QueryChange({limit})} 
+      min={20}  
+      max={100} 
+      step={5}  />;
+      const buildFilters = <BuildFilters  ref="branchTabs"  buildHistory={this.props.buildHistory} />;
+      return(<div className="align-center" >
+        <BuildHistoryTable buildFilters={buildFilters} countSlider={countSlider} builds ={builds}/>
+      </div>);
   }
 });
