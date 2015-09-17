fd81d66aff2e8c5fe4db4bf1e160571309c46076
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-16 19:40:46 -0500

    Fix Grouped builds view

--- src/main/jsx/components/job/BuildStep.jsx
+++ src/main/jsx/components/job/BuildStep.jsx
@@ -11,7 +11,8 @@ export default React.createClass({
     </div>
   },
   _compact(){
-    return <div className={"summary ui button circular compact-"+this.props.build.get('result')} key="summary"  data-number={this.props.build.get('number')} onClick={this._onClick} >{this._dotCiStep()}</div>
+    return <div className={"summary ui button circular compact-"+this.props.build.result} key="summary"  
+      data-number={this.props.build.number} onClick={this._onClick} >{this._dotCiStep()}</div>
   },
   _onClick(e){
     this.props.onClick(parseInt(e.target.getAttribute('data-number')));
@@ -20,9 +21,10 @@ export default React.createClass({
     return <iron-icon icon="arrow-forward"/>
   },
   _dotCiStep(){
-    return  this._isDownstreamBuild() ? this.props.build.get('parameters').filter(param => param.get('name')==='DOTCI_STEP').get(0).get('value'): this.props.build.get('number');
+    return  this._isDownstreamBuild() ? this.props.build.parameters
+    .filter(param => param.name ==='DOTCI_STEP')[0].value: this.props.build.number;
   },
   _isDownstreamBuild(){
-    return this.props.build.get('cause').get('name') === 'UPSTREAM';
+    return this.props.build.cause.name === 'UPSTREAM';
   }
 });
