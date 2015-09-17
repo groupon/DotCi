4aec84f8e38cd20f31bdec55096b7d4b4f25aa86
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-17 09:21:05 -0500

    Move actions into a different class

--- src/main/jsx/app.jsx
+++ src/main/jsx/app.jsx
@@ -1,27 +1,20 @@
 __webpack_public_path__= window.resURL+'js/';
 import React from "react";
-import ReactDOM from 'react-dom';
-import BuildHistoryPage from './pages/BuildHistoryPage.jsx';
-import {job,removeFilter,addFilter} from './api/Api.jsx'; 
 import  BuildHistory from './models/BuildHistory.js'
+import * as buildHistoryActions from './client/BuildHistoryPageActions.js';
+import {Router} from 'director';
 require('./app.css');
+import Drawer from './Drawer.jsx';
 window.onload = function (){
   const buildHistory = new BuildHistory();
   const actions = buildHistory.actions;
-  actions.DataChange.onAction = function(buildHistory){
-    ReactDOM.render(<BuildHistoryPage buildHistory={buildHistory}/>, document.getElementById('content'));
-  }
-  actions.QueryChange.onAction = function(buildHistory){
-    let query = buildHistory.query;
-    ReactDOM.render(<BuildHistoryPage buildHistory={buildHistory}/>, document.getElementById('content'));
-    job("buildHistoryTabs,builds[*,commit[*],cause[*],parameters[*]]",query.filter ,query.limit).then(data => {
-      actions.DataChange({...data, filters: data.buildHistoryTabs});
-    });
-  }
-  actions.RemoveFilter.onAction = removeFilter;
-  actions.AddFilter.onAction = addFilter;
+  actions.DataChange.onAction = buildHistoryActions.dataChange;
+  actions.QueryChange.onAction = buildHistoryActions.queryChange;
+  actions.RemoveFilter.onAction = buildHistoryActions.removeFilter;
+  actions.AddFilter.onAction = buildHistoryActions.addFilter;
 
   actions.QueryChange({filter: 'All', limit: 50});
+  // ReactDOM.render(<Drawer/>, document.getElementById('nav'));
 
 }
 
