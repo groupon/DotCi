7bd78a8571337e31a3ff9871f1f50da150122c5e
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-17 17:44:53 -0500

    Extract build actions into method

--- src/main/jsx/app.jsx
+++ src/main/jsx/app.jsx
@@ -6,25 +6,29 @@ import  BuildHistory from './models/BuildHistory.js'
 import * as buildHistoryActions from './client/BuildHistoryPageActions.js';
 require('./app.css');
 import Drawer from './Drawer.jsx';
-window.onload = function (){
+function getBuildHistoryActions(){
   const buildHistory = new BuildHistory();
   const actions = buildHistory.actions;
   actions.DataChange.onAction = buildHistoryActions.dataChange;
   actions.QueryChange.onAction = buildHistoryActions.queryChange;
   actions.RemoveFilter.onAction = buildHistoryActions.removeFilter;
   actions.AddFilter.onAction = buildHistoryActions.addFilter;
+  return actions;
+}
+window.onload = function (){
 
   const jobPath = jobUrl.replace(rootURL,'');
   const rootPath = window.location.pathname.split(jobPath)[0] +jobPath;
+  const buildHistoryActions = getBuildHistoryActions();
 
   page.base(rootPath);
   page('/','dotCIbuildHistory');
   page('dotCIbuildHistory', function () {
-    actions.QueryChange({filter: 'All', limit: 50});
+    buildHistoryActions.QueryChange({filter: 'All', limit: 50});
     ReactDOM.render(<Drawer menu="job"/>, document.getElementById('nav'));
   });
   page('dotCIbuildMetrics', function () {
-    actions.QueryChange({filter: 'All', limit: 0});
+    // actions.QueryChange({filter: 'All', limit: 0});
     ReactDOM.render(<Drawer menu="job"/>, document.getElementById('nav'));
   });
   page();
