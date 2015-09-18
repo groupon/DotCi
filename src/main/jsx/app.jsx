d7d6eca5044751c8607d7746cd9e8e737da4339a
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-18 15:13:37 -0500

    reorg actions

--- src/main/jsx/app.jsx
+++ src/main/jsx/app.jsx
@@ -3,28 +3,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import page from 'page';
 import  Model from './models/Model.js'
-import * as buildHistoryActions from './client/BuildHistoryPageActions.js';
+import bindBuildHistoryActions from './client/BuildHistoryPageActions.js';
 require('./app.css');
 import Drawer from './Drawer.jsx';
 function getBuildHistoryActions(){
   const buildHistory = new Model();
-  const actions = buildHistory.actions;
-  actions.DataChange.onAction = buildHistoryActions.dataChange;
-  actions.QueryChange.onAction = buildHistoryActions.queryChange;
-  actions.RemoveFilter.onAction = buildHistoryActions.removeFilter;
-  actions.AddFilter.onAction = buildHistoryActions.addFilter;
   return actions;
 }
 window.onload = function (){
 
   const jobPath = jobUrl.replace(rootURL,'');
   const rootPath = window.location.pathname.split(jobPath)[0] +jobPath;
-  const buildHistoryActions = getBuildHistoryActions();
+  const buildHistory = new Model();
+  bindBuildHistoryActions(buildHistory);
 
   page.base(rootPath);
   page('/','dotCIbuildHistory');
   page('dotCIbuildHistory', function () {
-    buildHistoryActions.QueryChange({filter: 'All', limit: 50});
+    buildHistory.actions.QueryChange({filter: 'All', limit: 50});
     ReactDOM.render(<Drawer menu="job"/>, document.getElementById('nav'));
   });
   page('dotCIbuildMetrics', function () {
--- src/main/jsx/client/BuildHistoryPageActions.js
+++ src/main/jsx/client/BuildHistoryPageActions.js
@@ -2,14 +2,22 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import BuildHistoryPage from './../pages/BuildHistoryPage.jsx';
 import {job} from './../api/Api.jsx'; 
-export {removeFilter,addFilter} from './../api/Api.jsx'; 
-export function dataChange(buildHistory){
+import  {removeFilter,addFilter} from './../api/Api.jsx'; 
+function dataChange(buildHistory){
   ReactDOM.render(<BuildHistoryPage buildHistory={buildHistory}/>, document.getElementById('content'));
 }
-export function queryChange(buildHistory){
+function queryChange(buildHistory){
   const actions = buildHistory.actions;
   let query = buildHistory.query;
   job("buildHistoryTabs,builds[*,commit[*],cause[*],parameters[*]]",query.filter ,query.limit).then(data => {
     actions.DataChange({...data, filters: data.buildHistoryTabs});
   });
+}
+
+export default function(buildHistory){
+  const actions = buildHistory.actions;
+  actions.DataChange.onAction = dataChange;
+  actions.QueryChange.onAction = queryChange;
+  actions.RemoveFilter.onAction = removeFilter;
+  actions.AddFilter.onAction = addFilter;
 }
