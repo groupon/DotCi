87fa15fd2b27ba754f71cf9d908536b02cebcd85
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-18 15:41:41 -0500

    Redo build metrics'

--- src/main/jsx/app.jsx
+++ src/main/jsx/app.jsx
@@ -4,18 +4,19 @@ import ReactDOM from "react-dom";
 import page from 'page';
 import  Model from './models/Model.js'
 import bindBuildHistoryActions from './client/BuildHistoryPageActions.js';
+import bindBuildMetricsActions from './client/BuildMetricsPageActions.js';
 require('./app.css');
 import Drawer from './Drawer.jsx';
-function getBuildHistoryActions(){
-  const buildHistory = new Model();
-  return actions;
-}
+//wiring
 window.onload = function (){
 
   const jobPath = jobUrl.replace(rootURL,'');
   const rootPath = window.location.pathname.split(jobPath)[0] +jobPath;
   const buildHistory = new Model();
   bindBuildHistoryActions(buildHistory);
+
+  const buildMetrics = new Model();
+  bindBuildMetricsActions(buildMetrics);
 
   page.base(rootPath);
   page('/','dotCIbuildHistory');
@@ -25,6 +26,7 @@ window.onload = function (){
   });
   page('dotCIbuildMetrics', function () {
     // actions.QueryChange({filter: 'All', limit: 0});
+    buildMetrics.actions.QueryChange({filter: 'All', limit: 50});
     ReactDOM.render(<Drawer menu="job"/>, document.getElementById('nav'));
   });
   page();
