e5cecf488b0d992126a06d9eaa945289804981c7
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-09-28 10:47:05 -0500

    Assign to top commiter

--- src/main/java/com/groupon/jenkins/github/PickCoderReviewer.java
+++ src/main/java/com/groupon/jenkins/github/PickCoderReviewer.java
@@ -27,16 +27,15 @@ public class PickCoderReviewer extends PostBuildNotifier {
         listener.getLogger().println("Picking code Reviewer");
         String repoUrl = build.getParent().getGithubRepoUrl();
         GHRepository repo = new GithubRepositoryService(repoUrl).getGithubRepository();
-        Map<String,Integer> count = new HashMap<String, Integer>();
+        Map<GHUser,Integer> count = new HashMap<GHUser, Integer>();
         try {
-            repo.getIssue(1).comment("meow");
             List<GHPullRequestFileDetail> files = repo.getPullRequest(1).listFiles().asList();
             for(GHPullRequestFileDetail file: files){
 //                String fileName = file.
                 GHCommitQueryBuilder commits = repo.queryCommits().path(file.getFilename());
                 List<GHCommit> commitList = commits.list().asList();
                 for(GHCommit commit : commitList){
-                    String commiter = commit.getCommitter().getLogin();
+                    GHUser commiter = commit.getCommitter();
                     if(count.containsKey(commiter)){
                         count.put(commiter, count.get(commiter)+1);
                     }else{
@@ -44,12 +43,27 @@ public class PickCoderReviewer extends PostBuildNotifier {
                     }
                 }
             }
-            ArrayList values = new ArrayList(count.values());
-          Collections.sort(values);
-
+            GHUser topCommiter = getTopCommiter(count);
+            listener.getLogger().println("Assigning issue to " + topCommiter.getLogin());
+            repo.getIssue(1).assignTo(topCommiter);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return false;
+    }
+
+    private GHUser getTopCommiter(Map<GHUser, Integer> counts) {
+        TreeMap<Integer,List<GHUser>> sorted =  new TreeMap<Integer, List<GHUser>>();
+        for(GHUser commiter: counts.keySet()){
+            Integer count = counts.get(commiter);
+            if(sorted.containsKey(count)){
+               sorted.get(count) .add(commiter);
+            }else{
+                ArrayList<GHUser> values = new ArrayList<GHUser>();
+                values.add(commiter);
+               sorted.put(count,values);
+            }
+        }
+        return sorted.firstEntry().getValue().get(0);
     }
 }
