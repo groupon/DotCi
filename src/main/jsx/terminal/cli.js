79fac5ed447ddcf8caa1a1fdc322b0c91da5cd04
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-11-15 11:09:40 -0600

    Add dotci binary

--- src/main/jsx/terminal/cli.js
+++ src/main/jsx/terminal/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 //--server http://localhost:8080/jenkins  --repo suryagaddipati/DotCi
 import terminal from './terminal.js';
 let {server,repo} =  require('minimist')(process.argv.slice(2));
