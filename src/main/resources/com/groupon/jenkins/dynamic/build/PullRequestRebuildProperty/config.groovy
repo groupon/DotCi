58f8bce0e11ae57e257d7d7da7845ac51b02f7d1
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-11-19 19:20:18 -0600

    Read pullrequest rebuild property only if its already set

--- src/main/resources/com/groupon/jenkins/dynamic/build/PullRequestRebuildProperty/config.groovy
+++ src/main/resources/com/groupon/jenkins/dynamic/build/PullRequestRebuildProperty/config.groovy
@@ -25,5 +25,5 @@ package com.groupon.jenkins.dynamic.build;
 def f=namespace(lib.FormTagLib)
 
 f.entry(title:_("Build Pull Requests from the same Repository"), field:"buildPullRequestsFromSameRepo") {
-    f.checkbox(name:"buildPullRequestsFromSameRepo", checked:instance.shouldRebuildPullRequestsFromSameRepo() )
+    f.checkbox(name:"buildPullRequestsFromSameRepo", checked: instance && instance.shouldRebuildPullRequestsFromSameRepo() )
 }
