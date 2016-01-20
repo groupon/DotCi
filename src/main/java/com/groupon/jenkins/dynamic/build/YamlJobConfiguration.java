bb1930786ae5bbf1cb1061c5db166b8801320f2e
Surya Gaddipati <surya.gaddipati@gmail.com>
2016-01-20 15:51:57 -0600

    Apply job Configuration from yaml configuration file

---
+++ src/main/java/com/groupon/jenkins/dynamic/build/YamlJobConfiguration.java
@@ -0,0 +1,91 @@
+package com.groupon.jenkins.dynamic.build;
+
+import hudson.Extension;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import hudson.model.Item;
+import hudson.model.JobProperty;
+import hudson.model.JobPropertyDescriptor;
+import hudson.model.listeners.ItemListener;
+import hudson.tasks.BuildWrapper;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.stapler.Stapler;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Extension
+public class YamlJobConfiguration extends ItemListener{
+    @Override
+    public void onCreated(Item item) {
+        if (item instanceof DynamicProject) {
+            try {
+                apply((DynamicProject) item);
+            } catch (IOException e) {
+               throw  new RuntimeException(e) ;
+            } catch (Descriptor.FormException e) {
+               throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public void apply(DynamicProject job) throws IOException, Descriptor.FormException {
+        try{
+            GHRepository repo = job.getGithubRepository();
+            GHContent configFile = repo.getFileContent(".ci.config.yml", repo.getMasterBranch());
+            Yaml yaml= new Yaml();
+            Map<String,Object> jobConfig= (Map) yaml.load(configFile.read());
+
+            Map<String, Descriptor<?>> jobProperties = getDescriptors(JobProperty.class);
+
+            for(String key: jobConfig.keySet()){
+                    JobPropertyDescriptor descriptor = (JobPropertyDescriptor) (jobProperties.containsKey(key)? jobProperties.get(key): jobProperties.get(key+"Property"));
+                    if(descriptor != null && descriptor.isApplicable(job.getClass())){
+                        JSONObject jsonConfig=new JSONObject();
+                        jsonConfig.accumulateAll((Map) jobConfig.get(key));
+                        JobProperty newProperty  = descriptor.newInstance(Stapler.getCurrentRequest(),jsonConfig);
+                        if(newProperty !=null){
+                            JobProperty oldProperty = job.getProperty(newProperty.getClass());
+                            job.removeProperty(oldProperty);
+                            job.addProperty(newProperty );
+                        }
+                    }
+
+            }
+//            Map<String, Descriptor<?>> jobWrappers = getDescriptors(BuildWrapper.class);
+//
+//
+////            for(BuildWrapper wrapper :  job.getBuildWrappersList()){
+////                Descriptor<BuildWrapper> descriptor = wrapper.getDescriptor();
+//////                descriptor.
+//////            descriptor.newInstance()
+////
+////            }
+            job.save();
+
+        }catch (FileNotFoundException _){
+           //do nothing
+        }
+
+    }
+
+
+    private <T extends Describable<T>,D extends Descriptor<T>> Map<String,Descriptor<?>> getDescriptors(Class<T> c) {
+        Jenkins jenkins = Jenkins.getActiveInstance();
+        Map<String,Descriptor<?>> r = new HashMap<String, Descriptor<?>>();
+        if (jenkins == null) {
+            return r;
+        }
+        for (Descriptor<?> d : jenkins.getDescriptorList(c)) {
+                r.put(d.clazz.getSimpleName(), d);
+        }
+        return r;
+    }
+}
